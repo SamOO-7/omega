24ad2583c40fa40c2be995cad642b8afaa97b6a8
Ivan Nikolsky <54115104+enty8080@users.noreply.github.com>
2020-02-28 13:32:55 +0300

    Create uninstall.sh

---
+++ uninstall.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+#            ---------------------------------------------------
+#                              Omega Framework                                
+#            ---------------------------------------------------
+#                  Copyright (C) <2020>  <Entynetproject>       
+#
+#        This program is free software: you can redistribute it and/or modify
+#        it under the terms of the GNU General Public License as published by
+#        the Free Software Foundation, either version 3 of the License, or
+#        any later version.
+#
+#        This program is distributed in the hope that it will be useful,
+#        but WITHOUT ANY WARRANTY; without even the implied warranty of
+#        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#        GNU General Public License for more details.
+#
+#        You should have received a copy of the GNU General Public License
+#        along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+RS="\033[1;31m"
+YS="\033[1;33m"
+CE="\033[0m"
+WHS="\033[0m"
+
+printf '\033]2;uninstall.sh\a'
+
+if [[ $EUID -ne 0 ]]
+then
+   sleep 1
+   echo -e ""$RS"[-] "$WHS"This script must be run as root!"$CE"" 1>&2
+   sleep 1
+   exit
+fi
+
+{
+rm /bin/omega
+rm /usr/local/bin/omega
+rm -rf ~/omega
+rm /data/data/com.termux/files/usr/bin/omega
+} &> /dev/null
