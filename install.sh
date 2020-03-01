09a1a68cc90be6fda8dde10903e7b4125bc99be1
Ivan Nikolsky <54115104+enty8080@users.noreply.github.com>
2020-03-01 13:42:27 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 #            ---------------------------------------------------
-#                              Omega Framework              
+#                              Omega Framework                                
 #            ---------------------------------------------------
-#                  Copyright (C) <2020>  <Entynetproject>
+#                  Copyright (C) <2020>  <Entynetproject>       
 #
 #        This program is free software: you can redistribute it and/or modify
 #        it under the terms of the GNU General Public License as published by
