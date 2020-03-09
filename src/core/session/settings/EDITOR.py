2632564424287061723391e320a3f2b5a522bb5d
Ivan Nikolsky <54115104+enty8080@users.noreply.github.com>
2020-03-09 14:40:39 +0300

    Update EDITOR.py

--- src/core/session/settings/EDITOR.py
+++ src/core/session/settings/EDITOR.py
@@ -1,14 +1,23 @@
-"""
-Set attacker's prefered text editor.
+#!/usr/bin/env python3
 
-* USE CASES:
+#            ---------------------------------------------------
+#                              Omega Framework                                
+#            ---------------------------------------------------
+#                  Copyright (C) <2020>  <Entynetproject>       
+#
+#        This program is free software: you can redistribute it and/or modify
+#        it under the terms of the GNU General Public License as published by
+#        the Free Software Foundation, either version 3 of the License, or
+#        any later version.
+#
+#        This program is distributed in the hope that it will be useful,
+#        but WITHOUT ANY WARRANTY; without even the implied warranty of
+#        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#        GNU General Public License for more details.
+#
+#        You should have received a copy of the GNU General Public License
+#        along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-# open TARGET setting content with EDITOR:
-> set TARGET +
-
-# use `edit` plugin to edit a remote file locally with EDITOR:
-> edit /var/www/html/index.php
-"""
 import os
 import linebuf
 import datatypes
