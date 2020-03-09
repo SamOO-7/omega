25c9fe95bbbb6ec3dfacc14c7680ae3c80c472aa
Ivan Nikolsky <54115104+enty8080@users.noreply.github.com>
2020-03-09 13:59:17 +0300

    Update payload.php

--- plugins/system/exec/payload.php
+++ plugins/system/exec/payload.php
@@ -1,3 +1,5 @@
+<?
+
 //            ---------------------------------------------------
 //                              Omega Framework                                
 //            ---------------------------------------------------
@@ -15,8 +17,6 @@
 //
 //        You should have received a copy of the GNU General Public License
 //        along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-<?
 
 !import(execute)
 
