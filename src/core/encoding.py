b1bfe97829e168ab3a884795fb50352f69f1fbec
Ivan Nikolsky <54115104+enty8080@users.noreply.github.com>
2020-03-09 14:23:51 +0300

    Update encoding.py

--- src/core/encoding.py
+++ src/core/encoding.py
@@ -1,8 +1,22 @@
-"""Generic encoding handler.
+#!/usr/bin/env python3
 
-This small string encoding/decoding manager aims to provide
-an uniform encoding handling policy for the omega framework.
-"""
+#            ---------------------------------------------------
+#                              Omega Framework                                
+#            ---------------------------------------------------
+#                  Copyright (C) <2020>  <Entynetproject>       
+#
+#        This program is free software: you can redistribute it and/or modify
+#        it under the terms of the GNU General Public License as published by
+#        the Free Software Foundation, either version 3 of the License, or
+#        any later version.
+#
+#        This program is distributed in the hope that it will be useful,
+#        but WITHOUT ANY WARRANTY; without even the implied warranty of
+#        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#        GNU General Public License for more details.
+#
+#        You should have received a copy of the GNU General Public License
+#        along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import sys
 import codecs
