c03db49e30041475983071b5ebe2b306fcfd207a
Ivan Nikolsky <54115104+enty8080@users.noreply.github.com>
2020-03-09 15:34:21 +0300

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,4 +1,24 @@
-"""Omega framework core loader.
+#!/usr/bin/env python3
+
+#            ---------------------------------------------------
+#                              Omega Framework                                
+#            ---------------------------------------------------
+#                  Copyright (C) <2020>  <Entynetproject>       
+#
+#        This program is free software: you can redistribute it and/or modify
+#        it under the terms of the GNU General Public License as published by
+#        the Free Software Foundation, either version 3 of the License, or
+#        any later version.
+#
+#        This program is distributed in the hope that it will be useful,
+#        but WITHOUT ANY WARRANTY; without even the implied warranty of
+#        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#        GNU General Public License for more details.
+#
+#        You should have received a copy of the GNU General Public License
+#        along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+"""Omega Framework core loader.
 
 This pseudo module is designed to be imported (import lib) from
 the omega script launcher (./omega).
