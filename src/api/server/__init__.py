05c7de28267f44e3089846cd9df1e3d1ee2e22c3
Ivan Nikolsky <54115104+enty8080@users.noreply.github.com>
2020-03-09 14:23:00 +0300

    Update __init__.py

--- src/api/server/__init__.py
+++ src/api/server/__init__.py
@@ -1,17 +1,22 @@
-"""Omega operations on target server.
+#!/usr/bin/env python3
 
-This module includes a collection of submodules
-designed for omega server-side operations.
-
-CONTENTS:
-
-* path (type: submodule)
-    Common operation on target server pathnames.
-
-* payload (type: submodule)
-    The omega payload request manager.
-"""
-
+#            ---------------------------------------------------
+#                              Omega Framework                                
+#            ---------------------------------------------------
+#                  Copyright (C) <2020>  <Entynetproject>       
+#
+#        This program is free software: you can redistribute it and/or modify
+#        it under the terms of the GNU General Public License as published by
+#        the Free Software Foundation, either version 3 of the License, or
+#        any later version.
+#
+#        This program is distributed in the hope that it will be useful,
+#        but WITHOUT ANY WARRANTY; without even the implied warranty of
+#        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#        GNU General Public License for more details.
+#
+#        You should have received a copy of the GNU General Public License
+#        along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 from . import path
 from . import payload
