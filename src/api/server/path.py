7db16d7bf05876c99259c6afcf429221c4f8c6a2
Ivan Nikolsky <54115104+enty8080@users.noreply.github.com>
2020-03-09 14:22:06 +0300

    Update path.py

--- src/api/server/path.py
+++ src/api/server/path.py
@@ -1,10 +1,23 @@
-"""Common operation on omega target server pathnames.
+#!/usr/bin/env python3
 
-These functions are designed for use in omega plugins,
-for normalizing and extracting server path components.
+#            ---------------------------------------------------
+#                              Omega Framework                                
+#            ---------------------------------------------------
+#                  Copyright (C) <2020>  <Entynetproject>       
+#
+#        This program is free software: you can redistribute it and/or modify
+#        it under the terms of the GNU General Public License as published by
+#        the Free Software Foundation, either version 3 of the License, or
+#        any later version.
+#
+#        This program is distributed in the hope that it will be useful,
+#        but WITHOUT ANY WARRANTY; without even the implied warranty of
+#        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#        GNU General Public License for more details.
+#
+#        You should have received a copy of the GNU General Public License
+#        along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-This module is highly inspired by `os.path` from standard library.
-"""
 import re
 from core import session
 
